2d21a55e966ef13b0a15fd0a3e7aa6d8f6541ab5
iPhilly <liufei.hehe@qq.com>
2018-06-04 10:02:30 +0800

    initial test app

---
+++ src/page/Demo.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default class Demo extends React.Component {
+    public render() {
+        return (
+            <h1>Hello TypeScript App</h1>
+        )
+    }
+}
